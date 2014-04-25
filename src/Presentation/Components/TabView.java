a2edfa5eca1cdf54a4bd9e629a55e305213328ed
Niels <Niels@10.153.0.133>
2014-04-25 12:26:12 +0200

    first commit of mainframe form

--- src/Presentation/Components/TabView.java
+++ src/Presentation/Components/TabView.java
@@ -6,10 +6,61 @@
 
 package Presentation.Components;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
+import javax.swing.border.BevelBorder;
+
 /**
  *
  * @author Niels
  */
-public class TabView {
+public class TabView extends JTabbedPane{
     
+    private int width, height;
+    ArrayList<Tab>  tabs;
+    
+    public TabView(int width, int height){
+        this.width = width;
+        this.height = height;
+        setSize(width, height);
+        tabs = new ArrayList<>();
+        
+    }
+    
+    public void addNewTab(String tabName, JPanel content){
+        content.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+        Tab t = new Tab(tabName, content);
+        tabs.add(t);
+        addTab("<html><body leftmargin=25 topmargin=10 marginwidth=25 marginheight=8>" + tabName + "</body></html>", content);
+        
+    }
+    
+    public void removeTab(JPanel content){
+        for(int i = 0; i < tabs.size(); i++){
+            if(tabs.get(i).getContent() == content) removeTabAt(i);
+        }
+    }
+    
+    
+    private class Tab{
+        
+        String tabName;
+        JPanel Content;
+        
+        
+        public Tab( String tabName, JPanel Content) {
+            this.Content = Content;
+            this.tabName = tabName;
+        }
+
+        public JPanel getContent() {
+            return Content;
+        }
+        
+        
+    }
 }
