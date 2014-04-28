d88c51c75ebbc4271665f7c5a436841d36fa63b3
Poul Nielsen <Poul Nielsen@MacPro>
2014-04-28 11:23:38 +0200

    xmlScanner

--- src/DAL/XmlScanner.java
+++ src/DAL/XmlScanner.java
@@ -7,11 +7,18 @@
 package DAL;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -28,9 +35,21 @@ public class XmlScanner {
     private void scanner()
     {
       
-        String xmlFile = "alarm_1.xml";
+        String xmlUrl = "https://www.dropbox.com/s/hf45d70vn7mtteh/alarm_1.xml";
         
+        try {
+            URL url = new URL(xmlUrl);
+            InputStream is = url.openStream();
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(is);
+            doc.getDocumentElement().normalize();
+            
+            NodeList location = doc.getElementsByTagName("currency");
         
+        } catch (IOException | ParserConfigurationException | SAXException ex) {
+            System.out.println("Nope, not going there!");
+        }
         
         
     }
