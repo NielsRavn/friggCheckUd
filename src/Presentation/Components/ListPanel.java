877c54b1e5ccb97d005e8406b4871c7a53352a16
Brobak <Brobak@10.153.0.148>
2014-04-30 12:42:18 +0200

    Merge origin/master
    Conflicts:
    	src/Presentation/Components/ListPanel.java

--- src/Presentation/Components/ListPanel.java
+++ src/Presentation/Components/ListPanel.java
@@ -11,6 +11,7 @@ import Presentation.MyColorConstants;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -46,6 +47,7 @@ public class ListPanel extends javax.swing.JPanel{
         model = new DefaultListModel();
         lstData.setModel(model);
         lstData.setCellRenderer(getClientListRenderer());
+        lstData.addListSelectionListener(new MyListSelectionListener());
         MyMouseMotionAdapter mma = new MyMouseMotionAdapter();
         lstData.addMouseListener(mma);
         jScrollPane1.addMouseListener(mma);
