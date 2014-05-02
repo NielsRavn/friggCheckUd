cb36caa0800474df5147f10085c4ac113a3b2d5b
Niels <Niels@10.153.0.133>
2014-05-02 10:08:05 +0200

    added header fixed alot of colors

--- src/Presentation/Frames/MainFrame.java
+++ src/Presentation/Frames/MainFrame.java
@@ -61,6 +61,7 @@ public class MainFrame extends javax.swing.JFrame {
     TabView tv;
     LogIn li;
     TimePicker tp;
+    Header head;
     
     ListPanel alarmPanel, carPanel, positionPanel, approveListPanel;
     
@@ -96,6 +97,9 @@ public class MainFrame extends javax.swing.JFrame {
             li = new LogIn(this);
             add(li, BorderLayout.CENTER);
             
+            head = new Header();
+            add(head, BorderLayout.NORTH);
+            
             //tv.setEnabledContent(p2, false);
         } catch (IOException ex) {
             JOptionPane.showMessageDialog(rootPane, "Could not use the config file, error: " + ex);
@@ -105,6 +109,7 @@ public class MainFrame extends javax.swing.JFrame {
     }
     
     public void createPanels(){
+        head.setUser(li.getFireman().getFirstName()+ " " + li.getFireman().getLastName());
         fot = new Footer(this);
         alarmPanel = getAlarmPanel();
         carPanel = getCarPanel();
@@ -302,6 +307,7 @@ public class MainFrame extends javax.swing.JFrame {
     public void logOut (){
         remove(tv);
         remove(fot);
+        head.loggedOut();
         if(tp != null) remove(tp);
         mfl.reset();
         add(li, BorderLayout.CENTER);
--- src/Presentation/MyConstants.java
+++ src/Presentation/MyConstants.java
@@ -25,8 +25,11 @@ public class MyConstants {
             COLOR_RED = new Color(106, 0, 0),
             COLOR_GREEN = new Color(0, 85, 0),
             COLOR_LIGHT_BLUE = new Color(180, 230, 255);
+    
     public static final Font FONT_BUTTON_FONT = new Font("Verdana", Font.PLAIN, 18);
+    public static final Font FONT_HEADER_TEXT = new Font("Verdana", Font.BOLD, 22);
     
     public static final Position DRIVER = new Position(2, "Chauffør"), TEAM_LEADER = new Position(1, "Holdleder"), FIREMAN = new Position(3, "Brandmand"), STATION_DUTY = new Position(4,"Stationsvagt");
     public static final Car STATION_DUTY_CAR = new Car(0, "res/Esbjerg_st.jpg", "Stations vagt", 5000);
+    
 }
