e13fd4d472de5a249b04f30d1fe3f6f072722a53
Niels Kristian Ravn <Niels Kristian Ravn@Nielses-pc>
2014-05-14 22:34:46 +0200

    story stuff new files

---
+++ src/DAL/Story_Access.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAL;
+
+import BE.Story;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Niels Kristian Ravn
+ */
+public class Story_Access extends DatabaseConnection{
+
+    public Story_Access() throws FileNotFoundException, IOException {
+    }
+
+    public void saveStory(Story story) throws SQLServerException, SQLException {
+        Connection con = null;
+        
+        try{
+            con = getConnection();
+            Statement stmnt = con.createStatement();
+            
+            int affectedRows = stmnt.executeUpdate("INSERT INTO Story ()");
+            
+            if(affectedRows == 0) throw new SQLException("input failed no affected rows");
+            
+        }finally{
+            if(con != null) con.close();
+        }
+    }
+    
+    
+    
+}
